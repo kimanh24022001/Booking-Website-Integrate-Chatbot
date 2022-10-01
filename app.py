920f9542c98405be88aba74c8a5b9fb090105c6d
Nguyễn Thị Kim Anh <79450471+kimanh24022001@users.noreply.github.com>
2022-10-01 12:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] =  "postgres://wnxgwnqsndjjkf:c5aedb553e95563cfe2bd7dab46e4607e0d99947f62f39b3189cdd2c2563ea68@ec2-3-219-19-205.compute-1.amazonaws.com:5432/d52nkfph56vp7n"
+app.config["SQLALCHEMY_DATABASE_URI"] =  "postgresql://wnxgwnqsndjjkf:c5aedb553e95563cfe2bd7dab46e4607e0d99947f62f39b3189cdd2c2563ea68@ec2-3-219-19-205.compute-1.amazonaws.com:5432/d52nkfph56vp7n"
 
 app.config['SECRET_KEY'] = "random string"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
